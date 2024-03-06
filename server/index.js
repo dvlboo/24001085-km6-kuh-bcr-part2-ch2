f90c56e5e29fd76f1ff433b4d7e05fb462ad2655
dvlboo <kuhhhckr@gmail.com>
2024-03-06 15:53:05 +0700

    Revert "try : http server and configure on public dir"
    This reverts commit 14e09e90a364e2a0448f5310378a3a9eb3eb306a.

--- server/index.js
+++ server/index.js
@@ -1,63 +1,10 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require('node : http')
+const port = 3001
 
-const PORT = 3001;
-
-const server = http.createServer((req, res) => {
-  let filePath;
-
-  // Handle requests dari url
-  if (req.url === '/' || req.url === '/index.html') {
-    filePath = path.join(__dirname, '../public/pages/index.html');
-  } else if (req.url === '/findCar') {
-    filePath = path.join(__dirname, '../public/pages/findCar.html');
-  } else if (req.url.endsWith('.css')) {
-    // serve css filenyaa
-    filePath = path.join(__dirname, '../public/css', req.url);
-  } else if (req.url.startsWith('/images')) {
-    // serve ke images filenyaa
-    filePath = path.join(__dirname, '../public', req.url);
-  } else if (req.url.startsWith('/scripts')) {
-    // serve JavaScript filenyaaa
-    filePath = path.join(__dirname, '../public', req.url);
-  } else {
-    // Handle 404 Not Found
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>404 Not Found</h1>');
-    return;
+http
+  .createServer ((req, res) => {
+    
   }
 
-  // console.log(req.url)
-
-  // read extentions file
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/html' });
-      res.end('<h1>500 Internal Server Error</h1>');
-      console.error(err);
-      return;
-    }
-
-    // mengubah ekstentions filenyaaa
-    let contentType = 'text/html';
-    if (filePath.endsWith('.css')) {
-      contentType = 'text/css';
-    } else if (filePath.endsWith('.png')) {
-      contentType = 'image/png';
-    } else if (filePath.endsWith('.jpg') || filePath.endsWith('.jpeg')) {
-      contentType = 'image/jpeg';
-    } else if (filePath.endsWith('.js')) {
-      contentType = 'text/javascript';
-    }
-
-    res.writeHead(200, { 'Content-Type': contentType });
-    res.end(content);
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-// jujurly pusing bikinyaaaaa 😅, tpi bisaaaa
+  
+)
